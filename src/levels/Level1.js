6a7b4de0080a3065f99a6b6cd1dfd5628558750d
Your Nameelaminhamidelameen <you@example.com>
2026-01-21 10:00:31 +0100

    .

--- src/levels/Level1.js
+++ src/levels/Level1.js
@@ -82,22 +82,22 @@ export default class Level1 extends Level {
 
         this.platforms = [
             // Marken (hela nivån)
-            new Platform(this.game, 0, height - 40, worldWidth, 40, '#654321'),
+            new Platform(this.game, 0, height - 40, worldWidth, 40, ''),
             
             // Plattformar (utspridda över nivån)
-            new Platform(this.game, 150, height - 140, 150, 20, '#8B4513'),
-            new Platform(this.game, 400, height - 200, 120, 20, '#8B4513'),
-            new Platform(this.game, 100, height - 280, 100, 20, '#8B4513'),
-            new Platform(this.game, 550, height - 160, 100, 20, '#8B4513'),
-            new Platform(this.game, 350, height - 320, 140, 20, '#8B4513'),
+            new Platform(this.game, 150, height - 140, 150, 20, ''),
+            new Platform(this.game, 400, height - 200, 120, 20, ''),
+            new Platform(this.game, 100, height - 280, 100, 20, ''),
+            new Platform(this.game, 550, height - 160, 100, 20, ''),
+            new Platform(this.game, 350, height - 320, 140, 20, ''),
             // Nya plattformar längre bort
-            new Platform(this.game, 900, height - 180, 140, 20, '#8B4513'),
-            new Platform(this.game, 1100, height - 240, 120, 20, '#8B4513'),
-            new Platform(this.game, 1300, height - 160, 100, 20, '#8B4513'),
-            new Platform(this.game, 1500, height - 280, 150, 20, '#8B4513'),
-            new Platform(this.game, 1750, height - 200, 120, 20, '#8B4513'),
-            new Platform(this.game, 1950, height - 320, 140, 20, '#8B4513'),
-            new Platform(this.game, 2150, height - 180, 100, 20, '#8B4513'),
+            new Platform(this.game, 900, height - 180, 140, 20, ''),
+            new Platform(this.game, 1100, height - 240, 120, 20, ''),
+            new Platform(this.game, 1300, height - 160, 100, 20, ''),
+            new Platform(this.game, 1500, height - 280, 150, 20, ''),
+            new Platform(this.game, 1750, height - 200, 120, 20, ''),
+            new Platform(this.game, 1950, height - 320, 140, 20, ''),
+            new Platform(this.game, 2150, height - 180, 100, 20, ''),
         ]
     }
 
